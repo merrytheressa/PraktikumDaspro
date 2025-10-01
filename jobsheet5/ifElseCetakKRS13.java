99fa422ec215ec4218fce21431256913cb37f6e3
merrytheressa <merrytheressa@gmail.com>
2025-10-01 12:41:03 +0700

    Tugas 1

--- jobsheet5/ifElseCetakKRS13.java
+++ jobsheet5/ifElseCetakKRS13.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ifElseCetakKRS13 {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.println("--- Cetak KRS SIAKAD");
+    System.out.println("--- Cetak KRS SIAKAD ---");
     System.out.print("Masukkan semester saat ini: ");
     int semester = sc.nextInt();
         if (semester == 1){
