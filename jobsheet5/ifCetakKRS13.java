84e96d6bc86a6295f9494ce3e999fb5cfbe39409
merrytheressa <merrytheressa@gmail.com>
2025-10-01 12:42:41 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS13.java
+++ jobsheet5/ifCetakKRS13.java
@@ -6,11 +6,18 @@ public class ifCetakKRS13 {
         System.out.println("--- Cetak KRS SIAKAD ---");
         System.out.print("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
-        if (uktLunas){
-            System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-        } else {
-            System.out.println ("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu");
-        }
+        System.out.println(
+            uktLunas
+                ? "Pembayaran UKT terverifikasi \n Silahkan cetak KRS dan minta tanda tangan DPA." 
+                : "Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu." 
+        );
+
+        //if (uktLunas){
+            //System.out.println("Pembayaran UKT terverifikasi");
+            //System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        //} else {
+            //System.out.println ("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu");
+        
     }
 }
+
